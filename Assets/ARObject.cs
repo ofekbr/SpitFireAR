c5ba326f64916ece9793f0158b8e3593f2cf415f
Ori Levy <ori294@gmail.com>
2019-10-26 18:31:06 +0300

    Init game

---
+++ Assets/ARObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARObject : MonoBehaviour
+{
+    private Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -70f) 
+        {
+            Strike();
+        }
+    }
+
+    void Strike()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0f, 0f, 0f);
+        transform.position = pos;
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //Make blast sound
+        //Recude live
+        Strike();
+    }
+}
---
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/CharacterControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterControll : MonoBehaviour
+{
+    public float speed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        if (!x.Equals(0) && !y.Equals(0))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+            Mathf.Atan2(x,y) * Mathf.Rad2Deg ,transform.eulerAngles.z);
+        }
+
+        if (!x.Equals(0) || !y.Equals(0))
+        {
+            Vector3 movement = new Vector3(x, 0, y);
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        else
+        {
+
+        }   
+    }
+
+    public void PlaceCharacter() 
+    {
+        transform.localPosition = Vector3.zero;
+    }
+}
